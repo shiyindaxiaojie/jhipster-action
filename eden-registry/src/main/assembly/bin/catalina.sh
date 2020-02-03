4d7ddd4c70c617e496663a2d720a5eb2bf677790
shiyindaxiaojie <1813986321@qq.com>
2020-02-04 00:47:09 +0800

    添加 bin 脚本

---
+++ eden-registry/src/main/assembly/bin/catalina.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+APP_NAME="registry"
+APP_DIR="../modules"
+APP_PATTERN="$APP_DIR/$APP_NAME*.jar"
+APP=`ls -l $APP_PATTERN | awk '${print $9}'`
+APP_SERVER=`ps -ef | grep java | grep $APP | grep -v grep | awk '{print $2}'`
+
+if [ $1 == "start" ]
+then
+    if [ -z "$JAVA_HOME" ]
+    then
+        echo JAVA_HOME: $JAVA_HOME
+        echo JAVA_VERSION:
+        java -version
+    fi
+    echo start $APP_NAME
+    nohup java -server Xms256m -Xmx1024m -XX:MaxPermSize=512m -jar $APP --spring.config.location=../config/ --logging.file=../logs/%APP_NAME%.log >/dev/null 2>&1 &
+elif [ $1 == "stop" ]
+then
+    echo stop $APP_NAME
+    kill -9 $APP_SERVER
+fi
---
+++ eden-uaa/src/main/assembly/bin/catalina.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set APP_NAME=uaa
+set APP_DIR=..\modules
+set APP_PATTERN=%APP_DIR%\%APP_NAME%*.jar
+for /f "delims=\" %%a in ('dir /b /a-d /o-d %APP_PATTERN%') do (
+     set APP=%APP_DIR%\%%a
+)
+
+if "%1" == "start" (
+     if exist "%JAVA_HOME%" (
+          echo JAVA_HOME: %JAVA_HOME%
+          echo JAVA_VERSION:
+          java -version
+     )
+     echo start %APP_NAME%
+     start "%APP_NAME%" java -server -Xms256m -Xmx1024m -XX:MaxPermSize=512m -jar %APP% --spring.config.location=../config/ --logging.file=../logs/%APP_NAME%.log
+) else if "%1" == "stop" (
+     echo stop %APP_NAME%
+     taskkill /fi "WINDOWTITLE eq %APP_NAME%"
+)
